d9ae354d71f245e85009c96007bd59df7bd422c8
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-04-30 13:18:13 +0200

    feat: updated templates to React 18

--- templates/react-ts/template/src/index.tsx
+++ templates/react-ts/template/src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
     <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+        <App />
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
--- templates/react/template/src/index.js
+++ templates/react/template/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
