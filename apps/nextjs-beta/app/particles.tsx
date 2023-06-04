1c7afc0a2253f263c7de626df13c4821a4d06a5c
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-06-04 14:59:02 +0200

    build: updated deps

--- apps/nextjs-beta/app/particles.tsx
+++ apps/nextjs-beta/app/particles.tsx
@@ -4,12 +4,12 @@ import { loadFull } from "tsparticles";
 import Particles from "react-particles";
 import { useCallback } from "react";
 import { Engine } from "tsparticles-engine";
-import { basic } from "tsparticles-demo-configs";
+import configs from "tsparticles-demo-configs";
 
 export default function ParticlesComponent(props: { id: string }) {
     const particlesInit = useCallback(async (engine: Engine) => {
         await loadFull(engine);
     }, []);
 
-    return <Particles id={props.id} init={particlesInit} options={basic}/>
+    return <Particles id={props.id} init={particlesInit} options={configs.basic} />;
 };
