c4ee76e4a4d56ab28547619435059433899c857d
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-10-02 01:12:40 +0200

    build: missing updates

--- components/react-new/lib/Particles.tsx
+++ components/react-new/lib/Particles.tsx
@@ -3,22 +3,26 @@ import type { IParticlesProps } from "./IParticlesProps";
 import { tsParticles, type Container } from "@tsparticles/engine";
 
 function Particles(props: IParticlesProps): JSX.Element {
-    const id = props.id ?? "tsparticles";
-    const [container, setContainer] = React.useState<Container | undefined>(undefined);
+  const id = props.id ?? "tsparticles";
+  const [container, setContainer] = React.useState<Container | undefined>(
+    undefined
+  );
 
-    useEffect(() => {
-        tsParticles.load({ id, url: props.url, options: props.options }).then(c => {
-            setContainer(c);
+  useEffect(() => {
+    tsParticles
+      .load({ id, url: props.url, options: props.options })
+      .then((c) => {
+        setContainer(c);
 
-            props.particlesLoaded?.(c);
-        });
+        props.particlesLoaded?.(c);
+      });
 
-        return () => {
-            container?.destroy();
-        };
-    }, [id, props.url, props.options]);
+    return () => {
+      container?.destroy();
+    };
+  }, [id, container, props, props.url, props.options]);
 
-    return <div id={id} className={props.className}></div>;
+  return <div id={id} className={props.className}></div>;
 }
 
 export default Particles;
