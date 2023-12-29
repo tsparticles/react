edd7f6c0d3ddeb0b6b43ced95599d46febe3ac2a
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-12-29 01:47:52 +0100

    build: called init function on the engine

--- components/react/lib/index.ts
+++ components/react/lib/index.ts
@@ -4,9 +4,11 @@ import Particles from "./Particles";
 export type { IParticlesProps } from "./IParticlesProps";
 
 export async function initParticlesEngine(
-  cb: (engine: Engine) => Promise<void>,
+    cb: (engine: Engine) => Promise<void>,
 ): Promise<void> {
-  await cb(tsParticles);
+    tsParticles.init();
+
+    await cb(tsParticles);
 }
 
 export default Particles;
