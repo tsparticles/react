ab0ffa7c963aa00d12b4e58a9d7d98581907723c
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2026-01-06 17:51:23 +0100

    refactor: using globalThis

--- templates/react-ts/template/src/serviceWorker.ts
+++ templates/react-ts/template/src/serviceWorker.ts
@@ -11,11 +11,11 @@
 // opt-in, read https://cra.link/PWA
 
 const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
+    globalThis.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    globalThis.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
+    globalThis.location.hostname.match(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
@@ -30,16 +30,16 @@ export function register(config?: Config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
         process.env.PUBLIC_URL,
-        window.location.href
+        globalThis.location.href
     );
-    if (publicUrl.origin !== window.location.origin) {
+    if (publicUrl.origin !== globalThis.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
 
-    window.addEventListener('load', () => {
+    globalThis.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
@@ -121,7 +121,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
           // No service worker found. Probably a different app. Reload the page.
           navigator.serviceWorker.ready.then(registration => {
             registration.unregister().then(() => {
-              window.location.reload();
+              globalThis.location.reload();
             });
           });
         } else {
