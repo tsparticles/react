b9dedf28eaf950d13512100f9ebe51b64d2423d0
unknown <durgaramannapureddy@gmail.com>
2026-02-16 16:24:54 +0530

    fix: forward style and other props to wrapper div

--- components/react/lib/Particles.tsx
+++ components/react/lib/Particles.tsx
@@ -12,16 +12,17 @@ const Particles: FC<IParticlesProps> = (props) => {
       .load({ id, url: props.url, options: props.options })
       .then((c) => {
         container = c;
-
         props.particlesLoaded?.(c);
       });
 
     return () => {
       container?.destroy();
     };
-  }, [id, props, props.url, props.options]);
+  }, [id, props.url, props.options]);
 
-  return <div id={id} className={props.className}></div>;
+  const { id: _ignored, particlesLoaded, options, url, ...rest } = props;
+
+  return <div id={id} {...rest} />;
 };
 
 export default Particles;
