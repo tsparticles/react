b8379ec65dfb7770bffdb7d0ff01959603c1a864
Matteo Bruni <matteo.bruni@ascom.com>
2026-04-09 21:15:07 +0200

    migrating to v4

--- apps/nextjs-beta/app/page.tsx
+++ apps/nextjs-beta/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Particles from "./particles";
 import { useEffect, useState } from "react";
